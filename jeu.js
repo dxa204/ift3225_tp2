f4a9261b104f65dc4b15029e69d2555fa67c0769
bbroux <alison.deblois@gmail.com>
2025-04-24 12:08:38 -0400

    premiere version

--- jeu.js
+++ jeu.js
@@ -1,57 +1,149 @@
-//générer la page de départ
-function generatePage() {
-  var login, pwd, score, mot;
+//get parametres dans URL
+function getParametres() {
+  //default params
+  const defaultParams = {
+    lang: "en",
+    tempsAlloue: 60,
+    hint: 10,
+  };
 
-  fetch("defintions.php/word")
-    .then((response) => response.json())
-    .then((data) => {
-      //choix d'un mot
-      mot = data[Math.floor(Math.random() * data.lenth)];
+  //get params
+  const parts = window.location.hash.split("/");
 
-      //insertion dans le document
-      document.getElementById("definition").textContent = mot.definition;
+  //verification
+  if (parts[1] !== "jeu" || parts[2] !== "word") {
+    return defaultParams;
+  }
 
-      //calcul du score alloue
-      score = mot.length * 10;
+  //si present dans url, sinon default
+  const lang = parts[3] || defaultParams.lang;
+  const tempsAlloue = parts[4] || defaultParams.tempsAlloue;
+  const hint = parts[5] || defaultParams.hint;
 
-      //initialiser avec bon nombre de lettres
-      var l = mot.length;
-      for (let i = 0; i < l; i++) {
-        let g = document.createElementWith("td");
-        g.setAttribute("id", "lettre{i}");
-        g.textContent = "_";
-        document.getElementById("longueur").appendChild();
-      }
-    })
-    .catch((error) => {
-      document.getElementById("definition").textContent =
-        "erreur: chargement impossible";
-    });
+  return { lang, tempsAlloue, hint };
+}
 
-  //initialisation du temps
-  document.getElementById("tempsRestant").textContent = temps;
-  lancerMinuterie(temps);
+//variables globales
+const params = getParametres();
+let mot = "";
+let score = 0;
+let scoreJoueur = 0;
+let affichageMot = [];
+let lang = params.lang;
+let tempsAlloue = params.tempsAlloue;
+let hint = params.hint;
+let termine = false;
+let interval = null;
 
-  //initialisation du joueur
-  fetch("gamers.php/session")
-    .then((response) => response.json)
-    .then((data) => {
-      login = data.login;
-      pwd = data.pwd;
-    });
+let progressBar = document.getElementById("points");
 
-  fetch("gamers.php/gamers")
-    .then((response) => response.json)
-    .then((data) => {
-      score = data.score;
-    });
+//get joueur that is logged
+fetch("gamers.php/session")
+  .then((response) => response.json())
+  .then((data) => {
+    login = data.login;
+    pwd = data.pwd;
+    document.getElementById("affichageNomJoueur").textContent = login;
+  });
 
-  document.getElementById("joueur").textContent = login;
-  document.getElementById("scoreJoueur").textContent = score;
+//get score du joueur courant
+fetch("gamers.php/gamers")
+  .then((response) => response.json())
+  .then((data) => {
+    scoreJoueur = data.score;
+    document.getElementById("scoreJoueur").textContent = scoreJoueur;
+  });
+
+//récupère un mot aléatoirement
+fetch("definition.php/word")
+  .then((response) => response.json())
+  .then((data) => {
+    entreeChoisie = data[Math.floor(Math.random() * data.length)];
+    mot = entreeChoisie.word.toUpperCase();
+    score = mot.length * 10;
+    progressBar.setAttribute("max", score);
+    affichageMot = Array(mot.length).fill("__");
+    ajusterAffichage();
+    lancerMinuterie();
+  })
+  .catch(
+    (error) =>
+      (document.getElementById("definition").textContent =
+        error + ": probleme de chargment de la définition")
+  );
+
+function ajusterAffichage() {
+  //enlever section mots présente
+  document.getElementById("affichageMot").textContent = "";
+
+  //barre de progres
+  progressBar.setAttribute("value", score);
+
+  //mot
+  let sectionMot = document.getElementById("affichageMot");
+  affichageMot.forEach((element, index) => {
+    const span = document.createElement("span");
+    span.textContent = element;
+    span.classList.add("entreeLettre");
+
+    if (element === "__") {
+      span.classList.add("clickable");
+      span.onclick = () => gestionEntreeLettre(index);
+    }
+
+    sectionMot.appendChild(span);
+    sectionMot.append(" ");
+  });
+
+  //score
+  document.getElementById("affichageScore").textContent = score;
+
+  //temps
+  document.getElementById("tempsRestant").textContent = tempsAlloue;
 }
 
 //gestion du temps
-function lancerMinuterie(temps) {
-  if (tempsRestant <= temps - 10) {
+function lancerMinuterie() {
+  interval = setInterval(() => {
+    tempsAlloue--;
+    document.getElementById("tempsRestant").textContent = `${tempsAlloue}s`;
+
+    if (tempsAlloue <= 0) {
+      terminerPartie(false);
+    }
+  }, 1000);
+}
+
+function gestionEntreeLettre(index) {
+  const lettreEntree = document
+    .getElementById("entreeLettre")
+    .value.toUpperCase();
+
+  //si cest pas une letttre on fait rien
+  let regex = /^[a-zA-Z]$/;
+  if (!lettreEntree.match(regex)) return;
+
+  if (mot[index] === lettreEntree) {
+    affichageMot[index] = lettreEntree;
+    score += 5;
+  } else score -= 5;
+
+  //remettre input value null (pour que joueur entre qqch autre)
+  document.getElementById("entreeLettre").value = "";
+
+  //afficher nouveau score/mot
+  ajusterAffichage();
+
+  //fin de la partie si mot termine
+  if (affichageMot.join("") === mot) {
+    finirLaPartie(true);
   }
+}
+
+function finirLaPartie(gagne) {
+  termine = true;
+  interval = null;
+  if (gagne) {
+    alert("Gagné. Bravo");
+  } else alert("Perdu.");
 }
